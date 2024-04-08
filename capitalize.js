6d0dc5f0e905acf95d76db26c4a4053579bad2fe
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2024-04-09 00:15:44 +0800

    clean: clean up root directory

--- capitalize.js
+++
@@ -1,6 +0,0 @@
-export default function capitalize(str) {
-  if (str === undefined) return '';
-  let regex = /^[a-zA-Z]+$/;
-  if (!regex.test(str.charAt(0))) return str;
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
